e9e9dc5c6f604c18ffd1a17f2ca1973c6842fe2a
Leo Singer <leo.singer@ligo.org>
2019-11-14 13:25:26 -0500

    Add docs

--- gracedb_sdk/__init__.py
+++ gracedb_sdk/__init__.py
@@ -17,33 +17,33 @@ def _hook_raise_errors(response, *args, **kwargs):
 
 
 class Client(API, SessionFileMixin, SessionAuthMixin, Session):
+    """GraceDB Client.
+
+    Parameters
+    ----------
+    url : str
+        GraceDB Client URL.
+    cert : str, tuple
+        Client-side X.509 certificate. May be either a single filename
+        if the certificate and private key are concatenated together, or
+        a tuple of the filenames for the certificate and private key.
+    username : str
+        Username for basic auth.
+    password : str
+        Password for basic auth.
+    force_noauth : bool, default=False
+        If true, then do not use any authentication at all.
+    fail_noauth : bool, default=False
+        If true, then raise an exception if authentication credentials are
+        not provided.
+    cert_reload : bool, default=False
+        If true, then automatically reload the client certificate before it
+        expires.
+    cert_cert_reload_timeout : int, default=300
+        Reload the certificate this many seconds before it expires.
+    """
 
     def __init__(self, url='https://gracedb.ligo.org/api/', *args, **kwargs):
-        """GraceDB Client.
-
-        Parameters
-        ----------
-        url : str
-            GraceDB Client URL.
-        cert : str, tuple
-            Client-side X.509 certificate. May be either a single filename
-            if the certificate and private key are concatenated together, or
-            a tuple of the filenames for the certificate and private key.
-        username : str
-            Username for basic auth.
-        password : str
-            Password for basic auth.
-        force_noauth : bool, default=False
-            If true, then do not use any authentication at all.
-        fail_noauth : bool, default=False
-            If true, then raise an exception if authentication credentials are
-            not provided.
-        cert_reload : bool, default=False
-            If true, then automatically reload the client certificate before it
-            expires.
-        cert_cert_reload_timeout : int, default=300
-            Reload the certificate this many seconds before it expires.
-        """
         super().__init__(url, *args, **kwargs)
         self.url = url
         self.headers['User-Agent'] = '{}/{}'.format(__name__, __version__)
