1d901a8c08efc6f5409abb34047b97afe18c9b6a
Leo Singer <leo.singer@ligo.org>
2019-11-14 14:24:47 -0500

    Document authentication sources

--- gracedb_sdk/__init__.py
+++ gracedb_sdk/__init__.py
@@ -17,7 +17,7 @@ def _hook_raise_errors(response, *args, **kwargs):
 
 
 class Client(API, SessionFileMixin, SessionAuthMixin, Session):
-    """GraceDB Client.
+    """GraceDB client session.
 
     Parameters
     ----------
@@ -41,7 +41,44 @@ class Client(API, SessionFileMixin, SessionAuthMixin, Session):
         expires.
     cert_cert_reload_timeout : int, default=300
         Reload the certificate this many seconds before it expires.
-    """
+
+    Notes
+    -----
+
+    When a new Client instance is created, the following sources of
+    authentication are tried, in order:
+
+    1.  If the :obj:`force_noauth` keyword argument is true, then perform no
+        authentication at all.
+
+    2.  If the :obj:`cert` keyword argument is provided, then use X.509 client
+        certificate authentication.
+
+    3.  If the :obj:`username` and :obj:`password` keyword arguments are
+        provided, then use basic auth.
+
+    4.  Look for a default X.509 client certificate in:
+
+        a.  the environment variables :envvar:`X509_USER_CERT` and
+            :envvar:`X509_USER_KEY`
+        b.  the environment variable :envvar:`X509_USER_PROXY`
+        c.  the file :file:`/tmp/x509up_u{UID}`, where :samp:`{UID}` is your
+            numeric user ID
+        d.  the files :file:`~/.globus/usercert.pem` and
+            :file:`~/.globus/userkey.pem`
+
+    5.  Read the netrc file [1]_ located at :file:`~/.netrc`, or at the path stored
+        in the environment variable :envvar:`NETRC`, and look for a username
+        and password matching the hostname in the URL.
+
+    6.  If the :obj:`fail_noauth` keyword argument is true, and no
+        authentication source was found, then raise a :class:`ValueError`.
+
+    References
+    ----------
+    .. [1] The .netrc file.
+           https://www.gnu.org/software/inetutils/manual/html_node/The-_002enetrc-file.html
+    """  # noqa: E501
 
     def __init__(self, url='https://gracedb.ligo.org/api/', *args, **kwargs):
         super().__init__(url, *args, **kwargs)
