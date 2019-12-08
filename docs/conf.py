5548eb7bd39685ea0cf4226a3df126f6e451de19
Leo Singer <leo.singer@ligo.org>
2019-12-08 12:09:00 -0500

    Fix autodoc mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-autodoc_mock_imports = ['cryptography', 'requests', 'safe-netrc']
+autodoc_mock_imports = ['cryptography', 'requests', 'safe_netrc']
 for mod in []:
     try:
         importlib.import_module(mod)
