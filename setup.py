5fe7f8187d7701910b5617869b0232b92c22cfda
Leo Singer <leo.singer@ligo.org>
2019-12-08 12:02:39 -0500

    Remove docs-requirements.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(filename):
 
 setup_requires = ['setuptools >= 30.3.0']
 if {'build_sphinx'}.intersection(sys.argv):
-    setup_requires.extend(get_requirements('docs-requirements.txt'))
+    setup_requires.append('sphinx')
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
 
