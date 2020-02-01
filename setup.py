f04f46630d9f56c6f59a5c8d244f1e086a0d954a
Leo Singer <leo.singer@ligo.org>
2020-02-01 17:59:40 -0500

    Remove unused function

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 import sys
 
 import versioneer
-import pkg_resources
 from setuptools import setup
-
-
-def get_requirements(filename):
-    with open(filename, 'r') as f:
-        return [str(r) for r in pkg_resources.parse_requirements(
-            line for line in f if not line.startswith('-r'))]
-
 
 setup_requires = ['setuptools >= 30.3.0']
 if {'build_sphinx'}.intersection(sys.argv):
